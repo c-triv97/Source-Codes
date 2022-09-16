4b92ff5fc31e48093ee668a185bd14397aead830
c-triv97 <c.trivett.1@research.gla.ac.uk>
2022-09-16 13:25:32 +0100

    Update GDPR
    adding "not in" function

--- GDPR.R
+++ GDPR.R
@@ -21,8 +21,10 @@ find_files <- function(pat, replace, path, sep, skip){
 
 standard_error <- function(x) sd(x, na.rm = TRUE) / sqrt(length(na.omit(x))) 
 
-wd <- function(file_path = "PhD/"){
-  wd = gsub("PhD.*", "", getwd())
+wd <- function(file_path = "PhD/", sub="PhD.*"){
+  wd = gsub(sub, "", getwd())
   dir =  paste0(wd, file_path)
   setwd(dir)
 }
+
+`%!in%` <- negate(`%in%`)

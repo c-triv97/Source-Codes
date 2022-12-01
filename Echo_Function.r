3665ff5d00394dcf32c2751b8ceed039cf8fc44c
c-triv97 <c.trivett.1@research.gla.ac.uk>
2022-12-01 16:22:28 +0000

    Echo functions fixing
    adding "dplyr::summarise" to stop package conflicts not grouping variables

--- Echo_Function.r
+++ Echo_Function.r
@@ -1,10 +1,10 @@
 # functions for performing calculations from echo measurements in imageJ 
 library(tidyverse)
 
-calc_hr <- function(dat, animal_id, msec = 1286.67){  #dat is table containing ID and number of peaks per image
+calc_hr <- function(dat, msec = 1286.67){  #dat is table containing ID and number of peaks per image
   dat %>%
     group_by(animal_id)%>%
-    summarise(mean_peaks = mean(peaks)) %>%
+    dplyr::summarise(mean_peaks = mean(peaks)) %>%
     mutate(bpm = 60/(msec/mean_peaks)*1000)
 }
 
@@ -16,16 +16,13 @@ average_echo <- function(data, measures = c(AWTd, EDD, PWTd, AWTs, ESD, PWTs)){ 
 
 calculations <- function(data){ # ASE cube = 1.04*((IVSTd+LVIDd+PWTd)3–LVIDd3) Devereux correction = 0.8(ASEcube)+0.6
   data %>%
-    mutate(ASE = 1.04*(((AWTd*10)+(EDD*10)+(PWTd*10))^3 - (EDD*10)^3)) %>%
+    mutate(ASE = 1.04*(((awtd*10)+(edd*10)+(pwtd*10))^3 - (edd*10)^3)) %>%
     mutate(LVM = (0.8*(ASE)+0.6)/1000) %>%
-    mutate(FS = (EDD - ESD)/EDD*100) %>%
-    mutate(RWT = ((AWTd*10)+(PWTd*10))/(EDD*10)) %>% 
-    mutate(EDDV = (EDD^3)*1.047, 
-           ESDV = (ESD^3)*1.047) %>%
+    mutate(FS = (edd - esd)/edd*100) %>%
+    mutate(RWT = ((awtd*10)+(pwtd*10))/(edd*10)) %>% 
+    mutate(EDDV = (edd^3)*1.047, 
+           ESDV = (edd^3)*1.047) %>%
     mutate(SV = (EDDV - ESDV)/1000) %>%
     mutate(CO = (SV * bpm)/1000) %>%
     mutate(EF = SV/EDDV)
 }
-
-
-

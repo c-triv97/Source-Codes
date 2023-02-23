e87d93ed0ef52a35422f34763c94b0d7040819a6
c-triv97 <c.trivett.1@research.gla.ac.uk>
2023-02-23 11:14:36 +0000

    removing out of date
    removing unused functions

--- GSEA_functions.r
+++ GSEA_functions.r
@@ -1,100 +1,9 @@
 library(tidyverse)
-library(fgsea)
 library(biomaRt)
 library(ggplot2)
 library(ggpubr)
-library(goseq)
+library(clusterProfiler)
 
-goseq_object <- function(data,
-                         test_cats = c("GO:BP", "GO:MF", "GO:CC"), 
-                         comparison){
-    data = data
-
-    sig_genes = data$FDR < 0.01 & !is.na(data$FDR)
-    genes = as.integer(sig_genes)
-    names(genes) = data$ensembl_gene_id
-
-    pwf = nullp(genes, "mm10", "ensGene", bias.data = data$medianTxLength)
-    print(head(pwf))
-
-    goResults = list()
-
-    for (i in 1:length(test_cats)){
-
-        results = goseq(pwf,
-                        "mm10",
-                        "ensGene",
-                        test.cats = test_cats[i])
-    
-        plot <- results %>%
-                top_n(10, wt = -over_represented_pvalue) %>%
-                mutate(hitPerc = numDEInCat / numInCat * 100) %>%
-                ggplot(aes(x = hitPerc,
-                y = term,
-                color = over_represented_pvalue,
-                size = numDEInCat)) +
-                geom_point() +
-                expand_limits(x=0) +
-                labs(x = "Hits (%)", y = test_cats[i],
-                    color = "p value",
-                    size = "count", 
-                    subtitle = comparison)+
-                theme_pubr(base_size = 18, legend = "right") +
-                scale_size_continuous(range = c(10, 20))+
-                scale_color_viridis_c()
-
-        print(plot)
-        ggsave(plot, filename = paste0("plots/",
-                                       comparison,
-                                       i,
-                                       ".pdf", 
-                                       sep = ""),
-               width = 12)
-
-        goResults[[i]] = results
-    }
-
-    return(goResults)
-}
-
-fgsea_output <- function(dataframe, pathways, filename){
-
-    gseaDat = dataframe
-
-    ranks = gseaDat$logFC
-
-    names(ranks) = gseaDat$entrezgene_id
-
-    fgseaRes = fgsea::fgsea(pathways,
-                            ranks, 
-                            minSize = 15,
-                            maxSize = 500)
-
-    write.table(fgseaRes, file = paste0(filename, "fgsea.txt"), sep = "\t",
-                row.names = FALSE)
-
-    top10up = fgseaRes %>%
-                filter(ES > 0) %>%
-                top_n(10, wt = -padj)
-    
-    top10down = fgseaRes %>%
-                filter(ES < 0) %>%
-                top_n(10, wt = -padj)
-
-    all = bind_rows(top10up, top10down)
-
-    plot = ggplot(all, aes(y = pathway, x = NES, size = size)) +
-        geom_point(alpha = 0.6, aes(color = padj)) +
-        guides(size = guide_legend(ncol = 1),
-            color = "none") +
-        theme_pubr(legend = "right") +
-        scale_size_continuous(range = c(5, 10)) +
-        scale_color_continuous()
-
-    print(plot)
-
-    return(fgseaRes)
-}
 
 gse_clusterprofiler <- function(data, 
                                 organism,
@@ -143,7 +52,6 @@ kegg_clusterprofiler <- function(data, # input needs to have entrez ids
 
     gse <- gseKEGG(geneList = dat,
                    organism = organism,
-                   nPerm = 10000,
                    minGSSize = 3,
                    maxGSSize = 800,
                    pvalueCutoff = 0.05,
@@ -176,11 +84,13 @@ plotting_common <- function(datalist, comparison_groups, order, save = FALSE, n=
         group_by(comparison) %>%
         arrange(p.adjust) %>%
         slice_head(n = n)) +
-        geom_point(aes(x = -log(p.adjust), y = Description, size = NES,
-                        color = comparison),
-                alpha = 0.6) +
+        geom_point(aes(x = -log(p.adjust), y = fct_reorder(Description, p.adjust),
+                        color = NES, shape = comparison),
+                alpha = 0.6, 
+                size = 8) +
         theme_bw(base_size = 20) +
-        labs(subtitle = "Top 10 most significantly enriched GO terms in each comparison group")
+        labs(subtitle = "Top 10 most significantly enriched GO terms in each comparison group", 
+             y = "Description")
     
     print(last_plot())
 

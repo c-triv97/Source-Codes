24b1f20822312a9d58b680eb14c5964f4076d488
c-triv97 <c.trivett.1@research.gla.ac.uk>
2023-09-29 13:08:24 +0100

    Thesis theme
    the packages and theme for thesis writing and graphing

---
+++ ThesisTheme.r
@@ -0,0 +1,19 @@
+cran.packages <- c("tidyverse",
+                   "ggplot2",
+                   "ggrepel",
+                   "RColorBrewer",
+                   "stringr",
+                   "viridis",
+                   "data.table",
+                   "tidyfast",
+                   "rstatix",
+                   "ggpubr",
+                   "ggprism"
+)
+lapply(cran.packages, require, character.only = TRUE)
+
+theme_thesis <-  theme_light(base_size = 18) + theme(
+    panel.background = element_rect(fill='transparent'),
+    plot.background = element_rect(fill='transparent', color=NA),
+    prism.ticks.length.y = unit(4, "pt")
+    )

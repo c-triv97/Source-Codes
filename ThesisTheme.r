b4b20904a5ceade6faa10c1962b1863cef4e20f6
c-triv97 <81438109+c-triv97@users.noreply.github.com>
2023-10-13 08:48:42 +0100

    Update ThesisTheme.r

--- ThesisTheme.r
+++ ThesisTheme.r
@@ -1,3 +1,4 @@
+require(installr)
 cran.packages <- c("tidyverse",
                    "ggplot2",
                    "ggrepel",
@@ -10,7 +11,7 @@ cran.packages <- c("tidyverse",
                    "ggpubr",
                    "ggprism"
 )
-lapply(cran.packages, require, character.only = TRUE)
+lapply(cran.packages, require2, character.only = TRUE)
 
 theme_thesis <-  theme_light(base_size = 18) + theme(
     panel.background = element_rect(fill='transparent'),

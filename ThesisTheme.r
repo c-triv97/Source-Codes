f1e28d0cd86dfe608b304ec69162851ca98ee10c
c-triv97 <81438109+c-triv97@users.noreply.github.com>
2023-10-20 13:41:36 +0100

    Update ThesisTheme.r
    changing package load

--- ThesisTheme.r
+++ ThesisTheme.r
@@ -1,4 +1,4 @@
-require(installr)
+if (!require("pacman")) install.packages("pacman")
 cran.packages <- c("tidyverse",
                    "ggplot2",
                    "ggrepel",
@@ -12,20 +12,9 @@ cran.packages <- c("tidyverse",
                    "ggprism"
 )
 
-packages.load <- function(x) {
-  if(require(x)){
-    print(paste(x, " is loaded correctly"))
-} else {
-    print(paste(x, " is trying to install"))
-    install.packages(x)
-    if(require(x)){
-        print(paste(x, " is installed and loaded"))
-    } else {
-        stop("could not install")
-    }
-}
+pacman::p_load(cran.packages, character.only = TRUE)
               
-lapply(cran.packages, packages.load, character.only = TRUE)
+#lapply(cran.packages, packages.load, character.only = TRUE)
 
 theme_thesis <-  theme_light(base_size = 18) + theme(
     panel.background = element_rect(fill='transparent'),

c191b797d813f9ac4df41ccb8048f691787479a5
Cara Trivett <81438109+c-triv97@users.noreply.github.com>
2024-06-04 11:06:39 +0200

    added thesis theme for backwards compatibility
    code from thesis relies on this theme

---
+++ ThesisTheme.r
@@ -0,0 +1,26 @@
+if (!require("pacman")) install.packages("pacman")
+cran.packages <- c("tidyverse",
+                   "ggplot2",
+                   "ggrepel",
+                   "RColorBrewer",
+                   "stringr",
+                   "viridis",
+                   "data.table",
+                   "tidyfast",
+                   "rstatix",
+                   "ggpubr",
+                   "ggprism"
+)
+
+pacman::p_load(cran.packages, character.only = TRUE)
+              
+#lapply(cran.packages, packages.load, character.only = TRUE)
+
+theme_thesis <-  theme_light(base_size = 18) + theme(
+    panel.background = element_rect(fill='transparent'),
+    plot.background = element_rect(fill='transparent', color=NA),
+    prism.ticks.length.y = unit(4, "pt"), 
+    strip.background = element_rect(fill = "transparent"), 
+    strip.text = element_text(color = "black", size = 18, face = "bold.italic"), 
+    panel.spacing = unit(2, "lines")
+    )

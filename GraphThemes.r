a92d6085f250c45194b07ea31978e878c5d937d3
Cara Trivett <81438109+c-triv97@users.noreply.github.com>
2024-10-19 13:58:45 +0200

    Update GraphThemes.r
    adding paper theme

--- GraphThemes.r
+++ GraphThemes.r
@@ -24,3 +24,14 @@ theme_scientific <-  theme_light(base_size = 18) + theme(
     strip.text = element_text(color = "black", size = 18, face = "bold.italic"), 
     panel.spacing = unit(2, "lines")
     )
+
+theme_paper <-  theme_light(base_size = 18) + theme(
+    panel.background = element_rect(fill='transparent'),
+    panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+    plot.background = element_rect(fill='transparent', color=NA),
+    prism.ticks.length.y = unit(4, "pt"), 
+    strip.background = element_rect(fill = "transparent"), 
+    strip.text = element_text(color = "black", size = 18, face = "bold.italic"),
+    panel.border = element_blank(),
+    axis.line = element_line(colour = "grey"),
+    panel.spacing = unit(2, "lines"))
